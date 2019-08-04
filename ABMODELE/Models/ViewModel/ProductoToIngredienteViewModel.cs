36619c895cccabb7d39b8741347fbbcf8492f1ab
Alen Figueroa <alen.figueroa.m@mail.pucv.cl>
2019-08-04 01:22:49 -0400

    Acabada funcionalidad para agregar y quitar ingredientes de un producto

---
+++ ABMODELE/Models/ViewModel/ProductoToIngredienteViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ABMODELE.Models.ViewModel
+{
+    public class ProductoToIngredienteViewModel
+    {
+        public ProductoToIngrediente productoToIngrediente { get; set; }
+        public int index { get; set; }
+        public int ProductoId { get; set; }
+    }
+}
