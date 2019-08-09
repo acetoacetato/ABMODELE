eebc73da5d338a544b1205e930eba87b1998e3b9
Alen Figueroa <alen.figueroa.m@mail.pucv.cl>
2019-08-09 11:28:29 -0400

    Agregado index bonitido

---
+++ ABMODELE/Models/ViewModel/ProductoPersonalizadoPartialViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ABMODELE.Models.ViewModel
+{
+    public class ProductoPersonalizadoPartialViewModel
+    {
+        public ProductoPersonalizado productoPersonalizado { get; set; }
+        public int index { get; set; }
+        public int idOrden { get; set; }
+    }
+}
